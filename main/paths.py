8afcf6f2b684dd512ad23b3634fc42c555d11e6d
Alexander Zhu <alexcaco3@gmail.com>
2020-12-16 14:51:13 -0800

    Update paths.py

--- main/paths.py
+++ main/paths.py
@@ -3,9 +3,7 @@ import random
 def drawStraightPath():
     path = ""
     for i in range(10):
-        path += f"<DrawBlock x='0' y='1' z='{i}' type='diamond_block' />" \
-                f"<DrawBlock x='1' y='1' z='{i}' type='stone' />" \
-                f"<DrawBlock x='-1' y='1' z='{i}' type='stone' />"
+        path += f"<DrawBlock x='0' y='1' z='{i}' type='diamond_block' />"
 
     path += "<DrawBlock x='0' y='1' z='5' type='gold_block' />"
     path += "<DrawBlock x='0' y='1' z='10' type='emerald_block' />"
@@ -15,9 +13,7 @@ def drawStraightPath():
 def drawPath2():
     path = ""
     for i in range(10):
-        path += f"<DrawBlock x='0' y='1' z='{i}' type='diamond_block' />" \
-                f"<DrawBlock x='1' y='1' z='{i}' type='stone' />" \
-                f"<DrawBlock x='-1' y='1' z='{i}' type='stone' />"
+        path += f"<DrawBlock x='0' y='1' z='{i}' type='diamond_block' />"
 
     path += "<DrawBlock x='0' y='2' z='5' type='gold_block' />"
     path += "<DrawBlock x='0' y='2' z='10' type='emerald_block' />"
@@ -26,15 +22,10 @@ def drawPath2():
 def drawPath3():
     path = ""
     for i in range(5):
-        path += f"<DrawBlock x='0' y='1' z='{i}' type='diamond_block' />" \
-                f"<DrawBlock x='1' y='1' z='{i}' type='stone' />" \
-                f"<DrawBlock x='-1' y='1' z='{i}' type='stone' />"
+        path += f"<DrawBlock x='0' y='1' z='{i}' type='diamond_block' />"
     
     for i in range(6):
-        path += f"<DrawBlock x='{i}' y='1' z='5' type='diamond_block' />" \
-                f"<DrawBlock x='{i+1}' y='1' z='4' type='stone' />" \
-                f"<DrawBlock x='{i+1}' y='1' z='6' type='stone' />"
-    path += "<DrawBlock x='-1' y='1' z='5' type='stone' /><DrawBlock x='-1' y='1' z='6' type='stone' /><DrawBlock x='0' y='1' z='6' type='stone' />"
+        path += f"<DrawBlock x='{i}' y='1' z='5' type='diamond_block' />"
 
     path += "<DrawBlock x='0' y='1' z='5' type='gold_block' />"
     path += "<DrawBlock x='6' y='1' z='5' type='emerald_block' />"
@@ -45,8 +36,6 @@ def drawPath4():
     path = ""
     for i in range(7):
         path += f"<DrawBlock x='0' y='{i+1}' z='{i}' type='diamond_block' />" \
-                f"<DrawBlock x='1' y='1' z='{i}' type='stone' />" \
-                f"<DrawBlock x='-1' y='1' z='{i}' type='stone' />"
 
     path += "<DrawBlock x='0' y='4' z='3' type='gold_block' />"
     path += "<DrawBlock x='0' y='7' z='7' type='emerald_block' />"
@@ -61,4 +50,4 @@ def sixBsix():
             path += f"<DrawBlock x='{i}' y='0' z='{j}' type='stone' />"
             if(random.random() < rand_noise):
                 path += f"<DrawBlock x='{i}' y='1' z='{j}' type='stone' />"
-    path += f"<DrawBlock x='6' y='0' z='6' type='emerald_block' /><DrawBlock x='6' y='1' z='6' type='stone' />"
+    path += f"<DrawBlock x='6' y='0' z='6' type='emerald_block' /><DrawBlock x='6' y='1' z='6' type='air' />"
