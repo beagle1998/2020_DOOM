bb69b0da36bb389e83d7aeef24816ecfd744cf08
Alexander Zhu <alexcaco3@gmail.com>
2020-12-16 14:20:19 -0800

    Added 6x6 with random bumps

--- main/paths.py
+++ main/paths.py
@@ -1,3 +1,5 @@
+import random
+
 def drawStraightPath():
     path = ""
     for i in range(10):
@@ -49,3 +51,14 @@ def drawPath4():
     path += "<DrawBlock x='0' y='4' z='3' type='gold_block' />"
     path += "<DrawBlock x='0' y='7' z='7' type='emerald_block' />"
     return path
+
+
+rand_noise = 0.2
+def sixBsix():
+    path = ""
+    for i in range(7):
+        for j in range(7):
+            path += f"<DrawBlock x='{i}' y='0' z='{j}' type='stone' />"
+            if(random.random() < rand_noise):
+                path += f"<DrawBlock x='{i}' y='1' z='{j}' type='stone' />"
+    path += f"<DrawBlock x='6' y='0' z='6' type='emerald_block' /><DrawBlock x='6' y='1' z='6' type='stone' />"
